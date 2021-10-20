2925bf79bf57ecf0ff3aa1d9eb1939435273e0c1
aaron_anderson <aaron.arthur.anderson@gmail.com>
2021-10-20 15:47:16 -0700

    initial build

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,18 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+
+}
